b4faaca38f65271fb3a806be29370dfbcf0bd870
Thomhehe <pthom2345@gmail.com>
2025-10-26 00:13:41 +0700

    upload

--- utils/report_utils.py
+++ utils/report_utils.py
@@ -2,31 +2,26 @@ import os
 from datetime import datetime
 from openpyxl import Workbook, load_workbook
 
+_report_initialized = set()
+
 def write_report(filename, data_dict):
+
     os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-    if "Time" not in data_dict:
-        data_dict["Time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    ordered_data = {"Time": data_dict.pop("Time")}
-    ordered_data.update(data_dict)
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data_dict = {"Time": time_now, **data_dict}
 
-    if not os.path.exists(filename):
+    # Tạo mới nếu file chưa khởi tạo
+    if filename not in _report_initialized or not os.path.exists(filename):
         wb = Workbook()
         ws = wb.active
         ws.title = "Report"
-        ws.append(list(ordered_data.keys()))
-        ws.append(list(ordered_data.values()))
-        wb.save(filename)
-        wb.close()
-        return
+        ws.append(list(data_dict.keys()))
+        _report_initialized.add(filename)
+    else:
+        wb = load_workbook(filename)
+        ws = wb.active
 
-    wb = load_workbook(filename)
-    ws = wb.active
-
-    ws.delete_rows(1, ws.max_row)
-
-    ws.append(list(ordered_data.keys()))
-    ws.append(list(ordered_data.values()))
-
+    ws.append(list(data_dict.values()))
     wb.save(filename)
     wb.close()
